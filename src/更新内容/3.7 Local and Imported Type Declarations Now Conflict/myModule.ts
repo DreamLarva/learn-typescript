666aa9420e9a55b77557b6e6fc70bffcd119db76
yangjiaqi <a13579>
2020-04-23 09:26:47 +0800

    2020/2/23

--- src/更新内容/3.7 Local and Imported Type Declarations Now Conflict/myModule.ts
+++ src/更新内容/3.7 Local and Imported Type Declarations Now Conflict/myModule.ts
@@ -1,9 +1,13 @@
 // ./myModule.ts
-import { SomeType } from "./someOtherModule";
+// import { SomeType } from "./someOtherModule"; // error
 export interface SomeType {
     x: number;
 }
 
+/**
+ * 现在引入的 interface 不再与本地的 interface 合并
+ * */
+
 function fn(arg: SomeType) {
-    console.log(arg.x); // Error! 'x' doesn't exist on 'SomeType'
+    // console.log(arg.x); // Error! 'x' doesn't exist on 'SomeType'
 }
