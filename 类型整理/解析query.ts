507e7f2d5e172fa78517e419e573ad93ebd50d18
yangjiaqi <yangjiaqi@orientsec.com.cn>
2022-06-28 16:58:02 +0800

    2022/6/28

--- 类型整理/解析query.ts
+++ 类型整理/解析query.ts
@@ -2,34 +2,29 @@
 type ParseParam<Param extends string> =
   Param extends `${infer Key}=${infer Value}`
     ? {
-      [K in Key]: Value
-    } : {};
+        [K in Key]: Value;
+      }
+    : {};
 
-type MergeValues<One, Other> =
-  One extends Other
-    ? One
-    : Other extends unknown[]
-      ? [One, ...Other]
-      : [One, Other];
+type MergeValues<One, Other> = One extends Other
+  ? One
+  : Other extends unknown[]
+  ? [One, ...Other]
+  : [One, Other];
 
-type MergeParams<
-  OneParam extends object,
-  OtherParam extends object
-  > = {
-  [Key in keyof OneParam | keyof OtherParam]:
-  Key extends keyof OneParam
+type MergeParams<OneParam extends object, OtherParam extends object> = {
+  [Key in keyof OneParam | keyof OtherParam]: Key extends keyof OneParam
     ? Key extends keyof OtherParam
       ? MergeValues<OneParam[Key], OtherParam[Key]>
       : OneParam[Key]
     : Key extends keyof OtherParam
-      ? OtherParam[Key]
-      : never
-}
+    ? OtherParam[Key]
+    : never;
+};
 
 type ParseQueryString<Str extends string> =
   Str extends `${infer Param}&${infer Rest}`
     ? MergeParams<ParseParam<Param>, ParseQueryString<Rest>>
     : ParseParam<Str>;
 
-
-type ParseQueryStringResult = ParseQueryString<'a=1&a=2&b=2&c=3&a=4'>;
+type ParseQueryStringResult = ParseQueryString<"a=1&a=2&b=2&c=3&a=4">;
